6aa67b87c84ba0a08c1c10d76bfc5d614c298c60
Mintakai <toni19129@student.hamk.fi>
2021-02-28 00:13:28 +0200

    updates

--- rentacar/urls.py
+++ rentacar/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('caradd/', views.caradd, name='caradd'),
     path('carlist/', views.carlist, name='carlist'),
-    path('carrent/', views.carrent, name='carrent'),
+    path('carrent/<int:pk>/', views.carrent, name='carrent'),
 ]
