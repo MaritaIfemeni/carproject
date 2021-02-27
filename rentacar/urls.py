e64ebd75f85efc45447055cc15b2710ff074cd8b
Mintakai <toni19129@student.hamk.fi>
2021-02-28 00:27:14 +0200

    updates

--- rentacar/urls.py
+++ rentacar/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('caradd/', views.caradd, name='caradd'),
     path('carlist/', views.carlist, name='carlist'),
     path('cardetails/<int:pk>/', views.cardetails, name='cardetails'),
+    path('carnotfound/<int:pk>/', views.cardetails, name='cardetails'),
 ]
