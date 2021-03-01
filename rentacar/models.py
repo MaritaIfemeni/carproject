6ec929b51ca6efcfb0569cb3a5458563fca849d8
Mintakai <toni19129@student.hamk.fi>
2021-03-01 14:20:09 +0200

    updates

--- rentacar/models.py
+++ rentacar/models.py
@@ -17,6 +17,10 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
+class CarOwner(models.Model):
+    user_id = models.ForeignKey('CustomUser', on_delete=models.CASCADE)
+    car_id = models.ForeignKey('Car'), on_delete=models.CASCADE)
+
 class Car(models.Model):
     carNumber = models.AutoField(primary_key=True)
     make = models.CharField(max_length=50)
