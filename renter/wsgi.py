0427a776b8def0cb4c4f2031d1094726a86143b0
Mintakai <toni19129@student.hamk.fi>
2021-03-01 16:07:51 +0200

    updates

--- renter/wsgi.py
+++ renter/wsgi.py
@@ -5,9 +5,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+
+path = '/home/mintai/renter'
+
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'renter.settings')
 
