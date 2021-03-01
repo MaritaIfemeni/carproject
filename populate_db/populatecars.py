ebe9138546df170e849d3a622bf113c2c7f22c73
Mintakai <toni19129@student.hamk.fi>
2021-03-01 17:14:49 +0200

    updates

---
+++ populate_db/populatecars.py
@@ -0,0 +1,45 @@
+import django
+django.setup()
+
+import random
+from rentacar.models import Car
+
+alphabet = ["a","b","c","d","e","f","g","e","h","i","j","k","l",
+            "m","n","o","p","q","r","s","t","y","v","w","x","y","z"]
+
+powerLines = ["Gasoline", "Diesel", "Hybrid", "Electric"]
+
+with open("populate_db/brands.txt", "r") as f:
+    content = f.read()
+
+brands = content.split(',')
+
+with open("populate_db/kunnat.txt", "r") as f:
+    locations_content = f.read()
+
+locations = locations_content.split(',')
+
+for i in range(100):
+    vmake = brands[random.randint(0,len(brands)-1)]
+    with open(f"populate_db/{vmake}.txt", "r") as f:
+        model_content = f.read()
+
+    models = model_content.split(',')
+    vmodel = models[random.randint(0,len(models)-1)]
+    print(f"{vmake} {vmodel}")
+
+    vregisterNum = ""
+
+    for i in range(3):
+        let = alphabet[random.randint(0,len(alphabet)-1)]
+        vregisterNum += let
+
+    vregisterNum += f"-{random.randint(1,999)}"
+
+    vlocation = locations[random.randint(0,len(locations)-1)]
+
+    Car.objects.bulk_create([
+        Car(make=vmake, model=vmodel, registerNum=vregisterNum, year=random.randint(1990,2020),
+            powerLine=powerLines[random.randint(0,3)], emissions=random.randint(100,300), 
+            seats=random.randint(2,7), location=vlocation),
+    ])
