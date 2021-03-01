efccb97389f872d51b9a8882831dde733b7b0aa2
Mintakai <toni19129@student.hamk.fi>
2021-03-01 17:00:48 +0200

    updates

--- rentacar/models.py
+++ rentacar/models.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
 
-class CustomUser(AbstractBaseUser):
+class CustomUser(AbstractUser):
     userNumber = models.AutoField(primary_key=True)
     email = models.CharField(max_length=50)
     phoneNum = models.CharField(max_length=50)
