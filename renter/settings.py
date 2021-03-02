fcc1980ecc013a318cc799d40cd7cf7b40953903
Mintakai <toni19129@student.hamk.fi>
2021-03-02 18:52:57 +0200

    updates

--- renter/settings.py
+++ renter/settings.py
@@ -124,6 +124,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
+MEDIA_URL = '/media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 AUTH_USER_MODEL = 'rentacar.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
