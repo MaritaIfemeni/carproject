f3c8fa9a482a3f4c71529e26370224c954fb99a5
Mintakai <toni19129@student.hamk.fi>
2021-03-02 21:24:29 +0200

    updates

--- rentacar/views.py
+++ rentacar/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse_lazy
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic.edit import CreateView
 
-from .forms import CustomUserCreationForm, CarForm, RentForm, CarImageForm, CarPickForm
+from .forms import CustomUserCreationForm, CarForm, RentForm, CarImageForm
 from .models import CustomUser, Car, Rent, Owner
 
 class SignUpView(CreateView):
