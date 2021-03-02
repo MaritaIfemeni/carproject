269c092ac4bb7892abb5d310adc05b226b7bef59
Mintakai <toni19129@student.hamk.fi>
2021-03-02 18:59:46 +0200

    updates

--- renter/urls.py
+++ renter/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
-from django.conf.urls.static import static
-from django.conf import settings
 
 urlpatterns = [
     path('rentacar/', include('django.contrib.auth.urls')),
     path('rentacar/', include('rentacar.urls')),
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
