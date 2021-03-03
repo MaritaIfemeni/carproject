a665ae566cba93eb0fa0db4733c0217aa33ae7bc
MaritaIfemeni <marita.ifemeni@student.hamk.fi>
2021-03-03 12:31:35 +0200

    button

--- static/rentacar/js/scripts.js
+++ static/rentacar/js/scripts.js
@@ -7,9 +7,9 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.display = "block";
+    mybutton.styles.display = "block";
   } else {
-    mybutton.display = "none";
+    mybutton.styles.display = "none";
   }
 }
 
