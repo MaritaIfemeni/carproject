7320977ec880e34df901695ff3c24c5f966ee3a9
MaritaIfemeni <marita.ifemeni@student.hamk.fi>
2021-03-03 12:14:01 +0200

    layout

--- static/rentacar/js/scripts.js
+++ static/rentacar/js/scripts.js
@@ -1,18 +1,7 @@
-//Get the button
+//Get the button "back to top"
 var mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-//window.onscroll = function() {scrollFunction()};
-
-//function scrollFunction() {
-  //if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    //mybutton.style.display = "block";
-  //} else {
-    //mybutton.style.display = "none";
-  //}
-//}
-
-// When the user clicks on the button, scroll to the top of the document
+// Onclick, scrolls to the top of the page
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
