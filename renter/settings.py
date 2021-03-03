b661b31916074a6a09a8f50b0010889a8298a992
MaritaIfemeni <marita.ifemeni@student.hamk.fi>
2021-03-03 11:17:00 +0200

    test

--- renter/settings.py
+++ renter/settings.py
@@ -35,10 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rentacar.apps.RentacarConfig',
-    'crispy_forms',
 ]
-
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
