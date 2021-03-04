7f4a754e772d133cfcd4cdee9a9073629a220698
Mintakai <toni19129@student.hamk.fi>
2021-03-04 16:18:36 +0200

    updates

--- rentacar/admin.py
+++ rentacar/admin.py
@@ -11,12 +11,6 @@ class CustomUserAdmin(UserAdmin):
     list_display = ['userNumber', 'username', 'first_name', 'last_name', 'email',
                     'phoneNum', 'address', 'postcode', 'city', 'country', 'paymentMethod',]
 
-class CustomCarAdmin(admin.ModelAdmin)
-    add_form = CustomCarCreationForm
-    form = CustomCarChangeForm
-    model = Car
-    list_display = ["make", "model"]
-
 # Register your models here.
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Car)
