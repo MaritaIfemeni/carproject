1bb49b3f8a329e20a38830ea43ed61b12b150ecd
Mintakai <toni19129@student.hamk.fi>
2021-03-04 04:11:22 +0200

    updates

--- rentacar/views.py
+++ rentacar/views.py
@@ -99,8 +99,8 @@ def account(request):
 
 @login_required
 def rents(request):
-    rents = Rent.objects.filter(renterNumber_id=request.user.userNumber).filter(~Q(expired=1))
-    exp_rents = rents = Rent.objects.filter(renterNumber_id=request.user.userNumber).filter(~Q(expired=0))
+    rents = Rent.objects.filter(renterNumber_id=request.user.userNumber)
+    exp_rents = rents = Rent.objects.filter(renterNumber_id=request.user.userNumber)
 
     context = {
         'rents': rents,
