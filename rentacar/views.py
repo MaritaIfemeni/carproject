9a240cb78144a14f15192aab3197ed98789b3978
Mintakai <toni19129@student.hamk.fi>
2021-03-06 03:35:35 +0200

    updates

--- rentacar/views.py
+++ rentacar/views.py
@@ -102,7 +102,7 @@ def coownership(request, pk):
     new_owner = AddOwner.objects.get(car=car)
 
     owner = Owner()
-    owner.assign_owner(car, new_owner.new_owner_id)
+    owner.assign_owner(car, new_owner.new_owner)
 
     car.pending = False
     car.save()
