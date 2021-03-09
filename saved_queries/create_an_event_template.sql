299e7d5c61256add1630477f25858a4abe65b945
Mintakai <toni19129@student.hamk.fi>
2021-03-09 20:54:54 +0200

    updates

--- saved_queries/create_an_event_template.sql
+++ saved_queries/create_an_event_template.sql
@@ -1,9 +1,9 @@
-CREATE EVENT rent_manager
+CREATE EVENT rent_manager_2
 ON SCHEDULE
 EVERY 1 HOUR
-COMMENT 'Manages expired rents and car status.'
+COMMENT 'Manages expired rents.'
 DO
 UPDATE rentacar_car
 INNER JOIN rentacar_rent ON rentacar_car.carNumber = rentacar_rent.carNumber_id
-SET rentacar_rent.expired = IF(rentacar_rent.endDate < now(), 1,0), rentacar_car.status = IF(rentacar_rent.endDate < now(), 2,1)
-WHERE rentacar_rent.startDate < now() AND rentacar_rent.expired < 1;
+SET rentacar_rent.expired = IF(rentacar_rent.endDate < now() - interval 1 DAY, 2,1)
+WHERE rentacar_rent.expired = 1;
