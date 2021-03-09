04f91f5109f4d2922ded2b314cda819fea763bc1
Mintakai <toni19129@student.hamk.fi>
2021-03-09 19:48:33 +0200

    updates

--- saved_queries/alter_car_status_based_on_time.sql
+++ saved_queries/alter_car_status_based_on_time.sql
@@ -1,4 +1,4 @@
 UPDATE rentacar_car
 INNER JOIN rentacar_rent ON rentacar_car.carNumber = rentacar_rent.carNumber_id
-SET rentacar_car.status = IF(rentacar_rent.endDate < now(), 0,1), rentacar_rent.expired = IF(rentacar_rent.endDate < now(), 1,0)
-WHERE rentacar_rent.startDate < now()
+SET rentacar_car.status = IF(rentacar_rent.endDate > now(), 2,0), rentacar_rent.expired = IF(rentacar_rent.endDate < now(), 1,0)
+WHERE rentacar_rent.startDate > now()
